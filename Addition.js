659968447c8f70daac0a17bdbcb494fe8070bb42
santhosh <santhoshkumarjr01@gmail.com>
2022-06-06 15:38:50 +0530

    first commit

---
+++ Addition.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("enter number"))
+let num2 = Number(prompt("enter number"));
+let sum = num1 + num2
+console.log("the sum of "+ num1 + " and "+ num2 + " is " + sum);
+
+
+let num3 = parseInt(prompt("enter number"));
+let num4 = parseInt(prompt("enter number"));
+let sum1 = num3 + num4;
+console.log(`the sum of ${num3} and ${num4} is ${sum1}`);
+
+
---
+++ PrintingMethods.js
@@ -0,0 +1,7 @@
+//print hello world
+console.log("hello world")
+alert("hello world")
+document.write("hello world")
+document.writeln("hello world")
+confirm("hello world")
+prompt("hello world")
